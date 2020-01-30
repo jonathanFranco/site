cbc7ea84345abd930d371ed12cd7b83b5e1ab3eb
jonathanFranco <jonathanfranco01@outlook.com>
2020-01-30 19:40:39 -0300

    add site

---
+++ js/sidebar.js
@@ -0,0 +1,34 @@
+//Mobile Menu Script
+function openNav() {
+  document.getElementById("myNav").style.width = "80%";
+  document.getElementById("menu-content").style.display = "initial";
+  $("#side-menu-nav,li a i#menu-content, #social-media a").css({'display':'block'});
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("menu-content").style.display = "none";
+  $("#side-menu-nav,li a i#menu-content, #social-media a").css({'display':'none'});
+}
+
+//Navbar Animation
+ $(window).on('scroll', function () {
+      if ($(window).scrollTop()) {
+        $("#logo").css({ 'transform': 'scale(0.8)','transition':'0.2s'});
+        $(".nav-effect").css({ 'transform': 'scale(0.9)' , 'color': '#fff'});
+        $(".nav-effect").css({ 'transition': '0.3s' });
+      } else {
+        $("#logo").css({ 'transform': 'scale(1.0)','transition':'0.2s'});
+        $(".nav-effect").css({ 'transform': 'scale(1.0)' });
+        $(".nav-effect").css({ 'transition': '0.3s' });
+      }
+
+      if ($(window).scrollTop() <= 500) {
+        $("#btntop").css({ 'display': 'none', 'transition':'0.5s' });
+        $("#btn-fixed-style").css({ 'opacity': '0.3' });
+      }else{
+        $("#btntop").css({ 'display': 'block', 'transition':'0.5s' });
+        $("#btn-fixed-style").css({ 'opacity': '0.3' });
+      }
+
+    });
